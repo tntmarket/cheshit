6279573ed881a05ff28cbf66b91b19c43c48845c
Dave Lu <dave792321001730003212345@gmail.com>
2013-09-21 21:30:52 -0700

    make pieces draggable

--- src/app.js
+++ src/app.js
@@ -1,31 +1,35 @@
-var App = angular.module('cheshit', ['firebase', 'dragdrop']);
+var App = angular.module('cheshit', [/*'firebase',*/ 'dragdrop']);
 
-App.controller('Chess', ['$scope', 'angularFire',
+App.controller('Chess', ['$scope', /*'angularFire',*/
 function($scope, angularFire) {
-   var ref = new Firebase('https://cheshit.firebaseio.com/');
-   $scope.thing = {
-      cray: [
-         [1,2,3],
-         [2,3,4]
-      ]
-   };
+   //var ref = new Firebase('https://cheshit.firebaseio.com/');
    $scope.game = {
       board: [
-          [ 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R' ],
-          [ 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P' ],
-          [ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ],
-          [ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ],
-          [ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ],
-          [ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ],
-          [ 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p' ],
-          [ 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r' ]
+         [ 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R' ],
+         [ 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P' ],
+         [ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ],
+         [ ' ', ' ', ' ', ' ', ' ', ' ', 'K', ' ' ],
+         [ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ],
+         [ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ],
+         [ 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p' ],
+         [ 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r' ]
       ],
       backup: [ 'P', 'P', 'R' ]
    };
 
-   $scope.gogo = function(piece) {
-      console.log('asdf');
-      console.log(piece);
+   $scope.$on('move', function(event, from, to) {
+      //console.log(arguments);
+      var board = $scope.game.board;
+
+      $scope.$apply(function() {
+         board[to[0]][to[1]] = board[from[0]][from[1]];
+         board[from[0]][from[1]] = ' ';
+      });
+   });
+
+   window.pg = function() {
+      console.log($scope.game.board);
    };
+
    //angularFire(ref, $scope, 'squares');
 }]);
