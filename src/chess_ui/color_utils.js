cd5a6698d9fb3747742724495c7823748381ec18
Dave Lu <dave792321001730003212345@gmail.com>
2013-10-01 01:52:50 -0700

    - make "directives and crap" require modules instead having the module require a initializer function returned by the "directives and crap" - explicitly require the needed "directives and crap" instead of the whole ChessUi module

--- src/chess_ui/color_utils.js
+++ src/chess_ui/color_utils.js
@@ -1,29 +1,30 @@
-define(function(){
-   function InitColorUtils(module) {
-      module.service('ColorUtils', function() {
+define([
+   'chess_ui/chess_ui'
+],
+function(ChessUi){
 
-         this.color = function(pieceType) {
-            if(pieceType === ' ') {
-               return false;
-            } else if(pieceType.toUpperCase() === pieceType) {
-               return 'white';
-            } else {
-               return 'black';
-            }
-         };
+   ChessUi.service('ColorUtils', function() {
 
-         this.opposite = function(color) {
-            if(color === 'white') {
-               return 'black';
-            } else if(color === 'black') {
-               return 'white';
-            } else {
-               throw (color + "isn't a color, noob");
-            }
-         };
+      this.colorOf = function(pieceType) {
+         if(pieceType === ' ') {
+            return false;
+         } else if(pieceType.toUpperCase() === pieceType) {
+            return 'white';
+         } else {
+            return 'black';
+         }
+      };
 
-      });
-   }
+      this.opposite = function(color) {
+         if(color === 'white') {
+            return 'black';
+         } else if(color === 'black') {
+            return 'white';
+         } else {
+            throw (color + "isn't a color, noob");
+         }
+      };
 
-   return InitColorUtils;
+   });
+
 });
